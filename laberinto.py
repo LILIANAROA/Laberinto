507212d3df61aca1eb197b19c052496f3b7cb8f6
LILIANAROA <roaliliana61@gmail.com>
2019-05-13 12:51:02 -0400

    como agrege los comandos

--- laberinto.py
+++ laberinto.py
@@ -1,1 +1,16 @@
 #dia 7
+
+print("hola que tal")
+
+##############
+
+#git add.
+#git commit -m "el mensaje"
+#git push       (Para empujar el codigo del Local al Remoto)
+#git pull        (Para estudiar el codigo del Remoto al local)
+
+#Comandos para navegar en la Terminal
+#############
+
+#ls o dir        (Para ver los archivos existentes)
+#cd nombre__de_-archivo   (Para entrar en el archivo)
